c45d31671943f8277cfff16b114feb4ba85ce8ed
Andy Cochran <acochran@council.nyc.gov>
2016-09-27 20:11:54 -0400

    move replaceByValue if values to arrays

--- gulpfile.js
+++ gulpfile.js
@@ -54,144 +54,75 @@ gulp.task('data', function() {
 
     function replaceByValue(field) {
       for( var k = 0; k < json.length; ++k ) {
-        if( "068" == json[k][field] ) { json[k][field] = "ADMIN FOR CHILDREN'S SERVICES";}
-        if( "073" == json[k][field] ) { json[k][field] = "BOARD OF CORRECTION";}
-        if( "003" == json[k][field] ) { json[k][field] = "BOARD OF ELECTIONS";}
-        if( "829" == json[k][field] ) { json[k][field] = "BUSINESS INTEGRITY COMMISSION";}
-        if( "010" == json[k][field] ) { json[k][field] = "BOROUGH PRESIDENT";}
-        if( "011" == json[k][field] ) { json[k][field] = "BOROUGH PRESIDENT";}
-        if( "012" == json[k][field] ) { json[k][field] = "BOROUGH PRESIDENT";}
-        if( "013" == json[k][field] ) { json[k][field] = "BOROUGH PRESIDENT";}
-        if( "014" == json[k][field] ) { json[k][field] = "BOROUGH PRESIDENT";}
-        if( "004" == json[k][field] ) { json[k][field] = "CAMPAIGN FINANCE BOARD";}
-        if( "103" == json[k][field] ) { json[k][field] = "CITY CLERK";}
-        if( "102" == json[k][field] ) { json[k][field] = "CITY COUNCIL";}
-        if( "042" == json[k][field] ) { json[k][field] = "CITY UNIVERSITY OF NEW YORK";}
-        if( "134" == json[k][field] ) { json[k][field] = 'CIVIL SERVICE COMMISSION';}
-        if( "054" == json[k][field] ) { json[k][field] = 'CIVILIAN COMPLAINT REVIEW BOARD';}
-        if( '226' == json[k][field] ) { json[k][field] = 'COMMISSION ON HUMAN RIGHTS';}
-        if( '341' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '342' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '343' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '344' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '345' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '346' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '347' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '348' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '349' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '350' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '351' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '352' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '381' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '382' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '383' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '384' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '385' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '386' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '387' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '388' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '389' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '390' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '391' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '392' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '431' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '432' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '433' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '434' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '435' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '436' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '437' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '438' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '439' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '440' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '441' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '442' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '443' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '444' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '471' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '472' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '473' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '474' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '475' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '476' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '477' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '478' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '479' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '480' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '481' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '482' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '483' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '484' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '485' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '486' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '487' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '488' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '491' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '492' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( '493' == json[k][field] ) { json[k][field] = 'COMMUNITY BOARD';}
-        if( "312" == json[k][field] ) { json[k][field] = "CONFLICTS OF INTEREST BOARD";}
-        if( "099" == json[k][field] ) { json[k][field] = "DEBT SERVICE";}
-        if( "125" == json[k][field] ) { json[k][field] = "DEPARTMENT FOR THE AGING";}
-        if( "810" == json[k][field] ) { json[k][field] = "DEPARTMENT OF BUILDINGS";}
-        if( "030" == json[k][field] ) { json[k][field] = "DEPARTMENT OF CITY PLANNING";}
-        if( "856" == json[k][field] ) { json[k][field] = "DEPARTMENT OF CITYWIDE ADMIN SERVICE";}
-        if( "866" == json[k][field] ) { json[k][field] = "DEPARTMENT OF CONSUMER AFFAIRS";}
-        if( "072" == json[k][field] ) { json[k][field] = "DEPARTMENT OF CORRECTION";}
-        if( "126" == json[k][field] ) { json[k][field] = "DEPARTMENT OF CULTURAL AFFAIRS";}
-        if( "850" == json[k][field] ) { json[k][field] = "DEPARTMENT OF DESIGN & CONSTRUCTION";}
-        if( "040" == json[k][field] ) { json[k][field] = "DEPARTMENT OF EDUCATION";}
-        if( "017" == json[k][field] ) { json[k][field] = "DEPARTMENT OF EMERGENCY MANAGEMENT";}
-        if( "826" == json[k][field] ) { json[k][field] = "DEPARTMENT OF ENVIRONMENTAL PROTECT.";}
-        if( "836" == json[k][field] ) { json[k][field] = "DEPARTMENT OF FINANCE";}
-        if( "816" == json[k][field] ) { json[k][field] = "DEPARTMENT OF HEALTH AND MENTAL HYGIENE";}
-        if( "071" == json[k][field] ) { json[k][field] = "DEPARTMENT OF HOMELESS SERVICES";}
-        if( "858" == json[k][field] ) { json[k][field] = "DEPARTMENT OF INFO TECH & TELECOMM";}
-        if( "032" == json[k][field] ) { json[k][field] = "DEPARTMENT OF INVESTIGATION";}
-        if( "846" == json[k][field] ) { json[k][field] = "DEPARTMENT OF PARKS AND RECREATION";}
-        if( "781" == json[k][field] ) { json[k][field] = "DEPARTMENT OF PROBATION";}
-        if( "860" == json[k][field] ) { json[k][field] = "DEPARTMENT OF RECORDS & INFORMATION SVS";}
-        if( "827" == json[k][field] ) { json[k][field] = "DEPARTMENT OF SANITATION";}
-        if( "801" == json[k][field] ) { json[k][field] = "DEPARTMENT OF SMALL BUSINESS SERVICES";}
-        if( "069" == json[k][field] ) { json[k][field] = "DEPARTMENT OF SOCIAL SERVICES";}
-        if( "841" == json[k][field] ) { json[k][field] = "DEPARTMENT OF TRANSPORTATION";}
-        if( "063" == json[k][field] ) { json[k][field] = "DEPARTMENT OF VETERANS' SERVICES";}
-        if( "260" == json[k][field] ) { json[k][field] = "DEPARTMENT OF YOUTH & COMMUNITY DEV";}
-        if( "901" == json[k][field] ) { json[k][field] = "DISTRICT ATTORNEY";}
-        if( "902" == json[k][field] ) { json[k][field] = "DISTRICT ATTORNEY";}
-        if( "903" == json[k][field] ) { json[k][field] = "DISTRICT ATTORNEY";}
-        if( "904" == json[k][field] ) { json[k][field] = "DISTRICT ATTORNEY";}
-        if( "905" == json[k][field] ) { json[k][field] = "DISTRICT ATTORNEY";}
-        if( "133" == json[k][field] ) { json[k][field] = "EQUAL EMPLOYMENT PRACTICES COMMISSION";}
-        if( "127" == json[k][field] ) { json[k][field] = "FINANCIAL INFORMATION SERVICE AGENCY";}
-        if( "057" == json[k][field] ) { json[k][field] = "FIRE DEPARTMENT";}
-        if( "819" == json[k][field] ) { json[k][field] = "HEALTH AND HOSPITALS CORP";}
-        if( "806" == json[k][field] ) { json[k][field] = "HOUSING PRESERVATION AND DEVELOPMENT";}
-        if( "132" == json[k][field] ) { json[k][field] = "INDEPENDENT BUDGET OFFICE";}
-        if( "136" == json[k][field] ) { json[k][field] = "LANDMARKS PRESERVATION COMM.";}
-        if( "025" == json[k][field] ) { json[k][field] = "LAW DEPARTMENT";}
-        if( "035" == json[k][field] ) { json[k][field] = "LIBRARIES";}
-        if( "037" == json[k][field] ) { json[k][field] = "LIBRARIES";}
-        if( "038" == json[k][field] ) { json[k][field] = "LIBRARIES";}
-        if( "039" == json[k][field] ) { json[k][field] = "LIBRARIES";}
-        if( "002" == json[k][field] ) { json[k][field] = "MAYORALTY";}
-        if( "098" == json[k][field] ) { json[k][field] = "MISCELLANEOUS";}
-        if( "156" == json[k][field] ) { json[k][field] = "NYC TAXI AND LIMOUSINE COMM";}
-        if( "820" == json[k][field] ) { json[k][field] = "OFFICE OF ADMIN TRIALS & HEARINGS";}
-        if( "021" == json[k][field] ) { json[k][field] = "OFFICE OF ADMINISTRATIVE TAX APPEALS";}
-        if( "313" == json[k][field] ) { json[k][field] = "OFFICE OF COLLECTIVE BARGAINING";}
-        if( "131" == json[k][field] ) { json[k][field] = "OFFICE OF PAYROLL ADMINISTRATION";}
-        if( "906" == json[k][field] ) { json[k][field] = "OFFICE OF PROSECUTION SPEC NARCO";}
-        if( "008" == json[k][field] ) { json[k][field] = "OFFICE OF THE ACTUARY";}
-        if( "015" == json[k][field] ) { json[k][field] = "OFFICE OF THE COMPTROLLER";}
-        if( "095" == json[k][field] ) { json[k][field] = "PENSION CONTRIBUTIONS";}
-        if( "056" == json[k][field] ) { json[k][field] = "POLICE DEPARTMENT";}
-        if( "941" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "942" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "943" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "944" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "945" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "945" == json[k][field] ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
-        if( "101" == json[k][field] ) { json[k][field] = "PUBLIC ADVOCATE";}
+        var agencyID = json[k][field];
+        if( ['068'] == agencyID ) { json[k][field] = "ADMIN FOR CHILDREN'S SERVICES";}
+        if( ['073'] == agencyID ) { json[k][field] = "BOARD OF CORRECTION";}
+        if( ['003'] == agencyID ) { json[k][field] = "BOARD OF ELECTIONS";}
+        if( ['829'] == agencyID ) { json[k][field] = "BUSINESS INTEGRITY COMMISSION";}
+        if( ['010','011','012','013','014'] == agencyID ) { json[k][field] = "BOROUGH PRESIDENT";}
+        if( ['004'] == agencyID ) { json[k][field] = "CAMPAIGN FINANCE BOARD";}
+        if( ['103'] == agencyID ) { json[k][field] = "CITY CLERK";}
+        if( ['102'] == agencyID ) { json[k][field] = "CITY COUNCIL";}
+        if( ['042'] == agencyID ) { json[k][field] = "CITY UNIVERSITY OF NEW YORK";}
+        if( ['134'] == agencyID ) { json[k][field] = 'CIVIL SERVICE COMMISSION';}
+        if( ['054'] == agencyID ) { json[k][field] = 'CIVILIAN COMPLAINT REVIEW BOARD';}
+        if( ['226'] == agencyID ) { json[k][field] = 'COMMISSION ON HUMAN RIGHTS';}
+        if( ['341','342','343','344','345','346','347','348','349','350','351','352','381',
+          '382','383','384','385','386','387','388','389','390','391','392','431','432','433',
+          '434','435','436','437','438','439','440','441','442','443','444','471','472','473',
+          '474','475','476','477','478','479','480','481','482','483','484','485','486','487',
+          '488','491','492','493'].indexOf(agencyID) != -1 ) { json[k][field] = "COMMUNITY BOARD";}
+        if( ['312'].indexOf(agencyID) != -1 ) { json[k][field] = "CONFLICTS OF INTEREST BOARD";}
+        if( ['099'].indexOf(agencyID) != -1 ) { json[k][field] = "DEBT SERVICE";}
+        if( ['125'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT FOR THE AGING";}
+        if( ['810'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF BUILDINGS";}
+        if( ['030'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF CITY PLANNING";}
+        if( ['856'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF CITYWIDE ADMIN SERVICE";}
+        if( ['866'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF CONSUMER AFFAIRS";}
+        if( ['072'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF CORRECTION";}
+        if( ['126'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF CULTURAL AFFAIRS";}
+        if( ['850'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF DESIGN & CONSTRUCTION";}
+        if( ['040'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF EDUCATION";}
+        if( ['017'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF EMERGENCY MANAGEMENT";}
+        if( ['826'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF ENVIRONMENTAL PROTECT.";}
+        if( ['836'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF FINANCE";}
+        if( ['816'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF HEALTH AND MENTAL HYGIENE";}
+        if( ['071'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF HOMELESS SERVICES";}
+        if( ['858'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF INFO TECH & TELECOMM";}
+        if( ['032'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF INVESTIGATION";}
+        if( ['846'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF PARKS AND RECREATION";}
+        if( ['781'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF PROBATION";}
+        if( ['860'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF RECORDS & INFORMATION SVS";}
+        if( ['827'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF SANITATION";}
+        if( ['801'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF SMALL BUSINESS SERVICES";}
+        if( ['069'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF SOCIAL SERVICES";}
+        if( ['841'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF TRANSPORTATION";}
+        if( ['063'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF VETERANS' SERVICES";}
+        if( ['260'].indexOf(agencyID) != -1 ) { json[k][field] = "DEPARTMENT OF YOUTH & COMMUNITY DEV";}
+        if( ['901','902','903','904','905'].indexOf(agencyID) != -1 ) { json[k][field] = "DISTRICT ATTORNEY";}
+        if( ['133'].indexOf(agencyID) != -1 ) { json[k][field] = "EQUAL EMPLOYMENT PRACTICES COMMISSION";}
+        if( ['127'].indexOf(agencyID) != -1 ) { json[k][field] = "FINANCIAL INFORMATION SERVICE AGENCY";}
+        if( ['057'].indexOf(agencyID) != -1 ) { json[k][field] = "FIRE DEPARTMENT";}
+        if( ['819'].indexOf(agencyID) != -1 ) { json[k][field] = "HEALTH AND HOSPITALS CORP";}
+        if( ['806'].indexOf(agencyID) != -1 ) { json[k][field] = "HOUSING PRESERVATION AND DEVELOPMENT";}
+        if( ['132'].indexOf(agencyID) != -1 ) { json[k][field] = "INDEPENDENT BUDGET OFFICE";}
+        if( ['136'].indexOf(agencyID) != -1 ) { json[k][field] = "LANDMARKS PRESERVATION COMM.";}
+        if( ['025'].indexOf(agencyID) != -1 ) { json[k][field] = "LAW DEPARTMENT";}
+        if( ['035','037','038','039'].indexOf(agencyID) != -1 ) { json[k][field] = "LIBRARIES";}
+        if( ['002'].indexOf(agencyID) != -1 ) { json[k][field] = "MAYORALTY";}
+        if( ['098'].indexOf(agencyID) != -1 ) { json[k][field] = "MISCELLANEOUS";}
+        if( ['156'].indexOf(agencyID) != -1 ) { json[k][field] = "NYC TAXI AND LIMOUSINE COMM";}
+        if( ['820'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF ADMIN TRIALS & HEARINGS";}
+        if( ['021'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF ADMINISTRATIVE TAX APPEALS";}
+        if( ['313'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF COLLECTIVE BARGAINING";}
+        if( ['131'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF PAYROLL ADMINISTRATION";}
+        if( ['906'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF PROSECUTION SPEC NARCO";}
+        if( ['008'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF THE ACTUARY";}
+        if( ['015'].indexOf(agencyID) != -1 ) { json[k][field] = "OFFICE OF THE COMPTROLLER";}
+        if( ['095'].indexOf(agencyID) != -1 ) { json[k][field] = "PENSION CONTRIBUTIONS";}
+        if( ['056'].indexOf(agencyID) != -1 ) { json[k][field] = "POLICE DEPARTMENT";}
+        if( ['941','942','943','944','945','945'].indexOf(agencyID) != -1 ) { json[k][field] = "PUBLIC ADMINISTRATOR";}
+        if( ['101'].indexOf(agencyID) != -1 ) { json[k][field] = "PUBLIC ADVOCATE";}
       }
       return json;
     }
